833762f7ec97cb0477f02fe0dfc186d05c2faa35
mapreducedog <26311063+mapreducedog@users.noreply.github.com>
2017-10-11 17:17:37 +0200

    iadsjdaij

--- user_interface.py
+++ user_interface.py
@@ -44,7 +44,7 @@ def add_docs():
     for flag in filter(lambda x: any(x[1][1]) and getattr(x[0], '__doc__'), itertools.chain(preprocess_flags, static_flags)):
         short_command = "-{1[0]}{args}, " if flag[1][0] else ""
         long_command = "--{1[1]}{args}  "
-        explanation = "\n\t{0.__doc__}\n"
+        explanation = ":\t{0.__doc__}\n"
         args = " <arg>..." if flag[2] else ""
         totstring = "".join([short_command, long_command, explanation])
         try:
